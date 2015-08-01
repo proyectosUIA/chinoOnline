f3118d84a64989529e2dd89f434c83b35b10667b
GeraldBarquero <gerald.bv1@gmail.com>
2015-07-31 18:29:15 -0600

    Editado el diseño de la página de Registro

--- ElChinoOnline/ElChinoOnline/Registro.aspx.vb
+++ ElChinoOnline/ElChinoOnline/Registro.aspx.vb
@@ -1,8 +1,15 @@
 ﻿Public Class Registro
     Inherits System.Web.UI.Page
+    Dim DB As New moduloBase
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
 
+    Protected Sub BtnRegistrar_Click(sender As Object, e As EventArgs) Handles BtnRegistrar.Click
+        If DB.DatoRepetidoDB("", "", TxtEmail.Text) Then
+
+        End If
+
+    End Sub
 End Class
