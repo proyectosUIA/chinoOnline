9261119a393a910f31a1053cef6caf598da2722b
GeraldBarquero <gerald.bv1@gmail.com>
2015-08-07 16:05:40 -0600

    fix 2

---
+++ ElChinoOnline/ElChinoOnline/Admin_productos.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Administrador.master" CodeBehind="Admin_productos.aspx.vb" Inherits="ElChinoOnline.Admin" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="Contenido1" runat="server">
+    <div id="mantenimiento-productos" class="row">
+        <div class="col m3 l4">
+            <br />
+        </div>
+        <div id="productos" class="col m3 l4" runat="server">
+            
+        </div>
+        <div class="col m3 l4">
+            <br />
+        </div>
+    </div>
+</asp:Content>
---
+++ ElChinoOnline/ElChinoOnline/Admin_productos.aspx.vb
@@ -0,0 +1,29 @@
+﻿Public Class Admin
+    Inherits System.Web.UI.Page
+
+    Private Sub Admin_PreInit(sender As Object, e As EventArgs) Handles Me.PreInit
+        'If Session("nombre") <> "" Then
+        '    If Session("tipousuario") = "1" Then
+        '        Session("pagina") = "Productos"
+
+        '    Else
+        '        Response.Redirect("Default.aspx")
+        '    End If
+        'Else
+        '    Response.Redirect("Default.aspx")
+
+        'End If
+        
+    End Sub
+
+    Protected Overrides Sub CreateChildControls()
+
+        Dim divcontainer As New HtmlGenericControl("div")
+
+        divcontainer.ID = "Contenido"
+        Me.Page.Form.Controls.Item(1).Controls.Item(1).Controls.Item(1).Controls.Add(divcontainer)
+
+
+    End Sub
+
+End Class
